649294451c89376c0add9709f8f6cbf90db48a86
OhadR <ohadr.developer@gmail.com>
2014-04-08 21:35:56 +0300

    keep supporting GAE DS - issue #2
    activating account

--- src/main/java/com/ohadr/dictionary/gae/GAEAuthenticationAccountRepositoryImpl.java
+++ src/main/java/com/ohadr/dictionary/gae/GAEAuthenticationAccountRepositoryImpl.java
@@ -8,13 +8,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.google.appengine.api.datastore.*;
 import com.ohadr.auth_flows.core.AbstractAuthenticationAccountRepository;
+import com.ohadr.auth_flows.interfaces.AuthenticationUser;
 import com.ohadr.auth_flows.mocks.InMemoryAuthenticationUserImpl;
 
 
 public class GAEAuthenticationAccountRepositoryImpl extends
 		AbstractAuthenticationAccountRepository 
 {
+	private static final String LOGIN_ATTEMPTS_LEFT_PROP_NAME = "loginAttemptsLeft";
+	private static final String ENABLED_PROP_NAME = "enabled";
+	private static final String LAST_PSWD_CHANGE_DATE_PROP_NAME = "lastPasswordChangeDate";
+
 	private static final String USER_DB_KIND = "User";
+
 
 	private static Logger log = Logger.getLogger(GAEAuthenticationAccountRepositoryImpl.class);
 
@@ -26,15 +32,15 @@ public class GAEAuthenticationAccountRepositoryImpl extends
 	}
 
 	@Override
-	public void setDisabled(String arg0) {
-		// TODO Auto-generated method stub
-		
+	public void setDisabled(String email) 
+	{
+		setEnabledFlag(email, false);
 	}
 
 	@Override
-	public void setEnabled(String arg0) {
-		// TODO Auto-generated method stub
-		
+	public void setEnabled(String email) 
+	{
+		setEnabledFlag(email, true);
 	}
 
 	@Override
@@ -46,13 +52,18 @@ public class GAEAuthenticationAccountRepositoryImpl extends
 	@Override
 	public void createUser(UserDetails user) 
 	{
+		AuthenticationUser authUser = (AuthenticationUser) user;
+
 		Entity dbUser = new Entity(USER_DB_KIND, user.getUsername());		//the username is the key
 
 		dbUser.setProperty("username", user.getUsername());
 		dbUser.setProperty("password", user.getPassword());
-		dbUser.setProperty("enabled", user.isEnabled());
+		dbUser.setProperty(ENABLED_PROP_NAME, user.isEnabled());
+		dbUser.setProperty(LOGIN_ATTEMPTS_LEFT_PROP_NAME, authUser.getLoginAttemptsLeft());
+		dbUser.setProperty(LAST_PSWD_CHANGE_DATE_PROP_NAME, new Date( System.currentTimeMillis()) );
 
-		datastore.put(dbUser);	}
+		datastore.put(dbUser);	
+	}
 
 	@Override
 	public void updateUser(UserDetails user) {
@@ -99,23 +110,47 @@ public class GAEAuthenticationAccountRepositoryImpl extends
 			return null;
 		}
 		
-		log.info("GAEAuthenticationAccountRepositoryImpl: got entity of " + username + ": " 
-				+ entity.getProperty("password") + entity.getProperty("isEnabled") 
-				+ entity.getProperty("LoginAttemptsLeft"));
-		
+		boolean isEnabled = false;
+		Object isEnabledObj = entity.getProperty(ENABLED_PROP_NAME);
+		if( null != isEnabledObj )
+		{
+			isEnabled = (Boolean)isEnabledObj;
+		}
+		int loginAttemptsLeft = 0;
+		Object loginAttemptsLeftObj = entity.getProperty(LOGIN_ATTEMPTS_LEFT_PROP_NAME);
+		if( null != loginAttemptsLeftObj )
+		{
+			loginAttemptsLeft = new Integer(loginAttemptsLeftObj.toString());
+		}
+
 		return new InMemoryAuthenticationUserImpl(
 						username, 
 						(String)entity.getProperty("password"),
-						(Boolean)entity.getProperty("isEnabled"),
-						(Integer)entity.getProperty("LoginAttemptsLeft"),
+						isEnabled,
+						loginAttemptsLeft,
 						(Date)entity.getProperty("PasswordLastChangeDate"));
 		
 	}
 
 	@Override
-	protected void setEnabledFlag(String arg0, boolean arg1) {
-		// TODO Auto-generated method stub
+	protected void setEnabledFlag(String username, boolean flag) 
+	{
+		Key userKey = KeyFactory.createKey(USER_DB_KIND, username);
+		Entity entity;
+		try 
+		{
+			entity = datastore.get(userKey);
+			log.info("GAEAuthenticationAccountRepositoryImpl: got entity of " + username + ": " + entity);
+		} 
+		catch (EntityNotFoundException e) 
+		{
+			log.error("GAEAuthenticationAccountRepositoryImpl: entity of " + username + " not found");
+//			throw new UsernameNotFoundException(e.getMessage(), e);
+			return;
+		}
 		
+		entity.setProperty(ENABLED_PROP_NAME, flag);				
+		datastore.put(entity);	
 	}
 
 	@Override
